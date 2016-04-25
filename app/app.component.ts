49f1a4427edc7ef312380b7d219af613eb0397a8
Anthony Williams <pull.rebase>
2016-04-24 20:57:33 -0600

    Added styles.css files

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,8 @@ import {Component} from 'angular2/core';
 
 
 @Component({
-	selector: "my-app",
-	templateUrl: "app/templates/splash-page.html"
+	selector: 'my-app',
+	templateUrl: 'app/templates/splash-page.html'
 })
 export class AppComponent {
 
